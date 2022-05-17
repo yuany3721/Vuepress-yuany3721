c1b39d436d5453b1ebc74136b6d67a8be790a9f0
yuany3721 <li1116@mail.ustc.edu.cn>
2022-05-17 13:25:33 +0800

    edit build dest to docs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: "Vuepress-reco-yuany3721",
     description: " ",
-    dest: "dist",
+    dest: "docs",
     head: [
         ["link", { rel: "icon", href: "/favicon.ico" }],
         ["meta", { name: "viewport", content: "width=device-width,initial-scale=1,user-scalable=no" }],
--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 ---
 home: true
-# tagline: 生活不止眼前的枸杞，还有蜂蜜和咖啡
 bgImageStyle: {
   height: '0'
 }
 isShowTitleInHome: false
-# actionText: Guide
-# actionLink: /views/other/guide
