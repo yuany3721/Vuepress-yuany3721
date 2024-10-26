dd370cfdd517e3c3db7d499043af307b7670ff70
yuany3721 <li1116@mail.ustc.edu.cn>
2024-10-27 00:06:12 +0800

    update

--- docs/assets/js/21.4f570ac0.js
+++ docs/assets/js/21.4f570ac0.js
@@ -1,5 +1,1 @@
-<<<<<<<< HEAD:docs/assets/js/21.4f570ac0.js
 (window.webpackJsonp=window.webpackJsonp||[]).push([[21],{587:function(t,e,n){"use strict";n.r(e);var s=n(17),l=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}})}),[],!1,null,null,null);e.default=l.exports}}]);
-========
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{588:function(t,e,n){"use strict";n.r(e);var s=n(17),l=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}})}),[],!1,null,null,null);e.default=l.exports}}]);
->>>>>>>> 130802e8005f378f3646a20772408923483a89b7:docs/assets/js/21.8bcb5be6.js
--- docs/assets/js/53.d5c26d46.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:docs/assets/js/53.d5c26d46.js
-(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{622:function(t,e,s){"use strict";s.r(e);var n=s(17),i=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[e("code",[this._v("sudo dpkg-reconfigure tzdata")])]),this._v(" "),e("p",[this._v("Asia --\x3e Shanghai")])])}),[],!1,null,null,null);e.default=i.exports}}]);
-========
-(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{623:function(t,e,s){"use strict";s.r(e);var n=s(17),i=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[e("code",[this._v("sudo dpkg-reconfigure tzdata")])]),this._v(" "),e("p",[this._v("Asia --\x3e Shanghai")])])}),[],!1,null,null,null);e.default=i.exports}}]);
->>>>>>>> 130802e8005f378f3646a20772408923483a89b7:docs/assets/js/53.1fc3d5c0.js
